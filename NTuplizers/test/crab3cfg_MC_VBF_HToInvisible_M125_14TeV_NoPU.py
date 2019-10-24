83423699a1e4a959f679f9c39ee12d1309121336
Marino Missiroli <m.missiroli@cern.ch>
2019-10-24 20:26:17 +0200

    [NTuplizers] updated test/crab3cfg_MC_VBF_HToInvisible_M125_14TeV_*.py

--- NTuplizers/test/crab3cfg_MC_VBF_HToInvisible_M125_14TeV_NoPU.py
+++ NTuplizers/test/crab3cfg_MC_VBF_HToInvisible_M125_14TeV_NoPU.py
@@ -9,7 +9,7 @@ config.General.transferLogs = True
 
 config.section_('JobType')
 config.JobType.pluginName  = 'Analysis'
-config.JobType.maxMemoryMB = 2500
+config.JobType.maxMemoryMB = 5000
 config.JobType.psetName = 'jmeTriggerNTuple_step3_CHSPFJets_11_0_0_pre7_cfg.py'
 config.JobType.inputFiles = ['step3_CHSPFJets_11_0_0_pre7.py']
 config.JobType.pyCfgParams = ['output=VBF_HToInvisible_M125_14TeV_NoPU.root']
@@ -21,9 +21,9 @@ config.Data.ignoreLocality = False
 config.Data.splitting = 'EventAwareLumiBased'
 config.Data.inputDataset = '/VBF_HToInvisible_M125_14TeV_powheg_pythia8/PhaseIITDRSpring19MiniAOD-NoPU_106X_upgrade2023_realistic_v3-v2/MINIAODSIM'
 config.Data.secondaryInputDataset = '/VBF_HToInvisible_M125_14TeV_powheg_pythia8/PhaseIITDRSpring19DR-NoPU_106X_upgrade2023_realistic_v3-v2/GEN-SIM-DIGI-RAW'
-config.Data.outLFNDirBase = '/store/user/missirol/jme_trigger/jmeTriggerNtuples/Phase2/191026/VBF_HToInvisible_M125_14TeV_NoPU'
+config.Data.outLFNDirBase = '/store/user/missirol/jme_trigger/jmeTriggerNtuples/Phase2/191024_v01/VBF_HToInvisible_M125_14TeV_NoPU'
 config.Data.unitsPerJob = 100
-config.Data.totalUnits = 100000
+config.Data.totalUnits = -1
 
 config.section_('Site')
 config.Site.storageSite = 'T2_DE_DESY'
