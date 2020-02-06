85389170131a1dbbc7f5af18c3c6eefa8eedaa9d
Marino Missiroli <marino.missiroli@desy.de>
2020-02-06 16:12:49 +0100

    [NTuplizers] updated test/run_htc_driver_200205.sh

--- NTuplizers/test/run_htc_driver_200205.sh
+++ NTuplizers/test/run_htc_driver_200205.sh
@@ -79,6 +79,18 @@ htc_driver -c jmeTriggerNTuple_cfg.py -n 100 numThreads=1 --cpus 1 --memory 3000
  pfdqm=1 trkdqm=1 reco=${TRKV}_110X_D49 gt=110X_mcRun4_realistic_v3
 
 htc_driver -c jmeTriggerNTuple_cfg.py -n 100 numThreads=1 --cpus 1 --memory 3000 --runtime 10800 \
+ -d /VBF_HToInvisible_M125_14TeV_powheg_pythia8/PhaseIITDRSpring19MiniAOD-NoPU_106X_upgrade2023_realistic_v3-v2/MINIAODSIM \
+ -o ${ODIR}_skimmedTracks/PhaseIITDRSpring19_VBF_HToInvisible_14TeV_NoPU \
+ -m ${NEVT} \
+ pfdqm=1 trkdqm=1 reco=${TRKV}_110X_D41 skimTracks=1
+
+htc_driver -c jmeTriggerNTuple_cfg.py -n 100 numThreads=1 --cpus 1 --memory 3000 --runtime 10800 \
+ -d /VBF_HToInvisible_M125_14TeV_powheg_pythia8/PhaseIITDRSpring19MiniAOD-PU200_106X_upgrade2023_realistic_v3-v1/MINIAODSIM \
+ -o ${ODIR}_skimmedTracks/PhaseIITDRSpring19_VBF_HToInvisible_14TeV_PU200 \
+ -m ${NEVT} \
+ pfdqm=1 trkdqm=1 reco=${TRKV}_110X_D41 skimTracks=1
+
+htc_driver -c jmeTriggerNTuple_cfg.py -n 100 numThreads=1 --cpus 1 --memory 3000 --runtime 10800 \
  -d /RelValQCD_Pt15To7000_Flat_14TeV/CMSSW_11_0_0-110X_mcRun4_realistic_v2_2026D49noPU-v1/MINIAODSIM -p 1 \
  -o ${ODIR}_skimmedTracks/RelVal1100_2026D49_QCD_Pt15To7000_Flat_14TeV_NoPU \
  -m ${NEVT} \
