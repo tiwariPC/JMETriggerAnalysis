5e3a8434a7d3cbe9b3bdb6dd6042d558c74865df
Marino Missiroli <marino.missiroli@desy.de>
2020-02-06 10:50:31 +0100

    [Common] plugins/plugins.cc -> plugins/PFCandidateHistogrammer.cc

--- Common/plugins/PFCandidateHistogrammer.cc
+++ Common/plugins/PFCandidateHistogrammer.cc
@@ -1,10 +1,10 @@
 #include "JMETriggerAnalysis/Common/plugins/PFCandidateHistogrammer.h"
-
+#include "FWCore/Framework/interface/MakerMacros.h"
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
 #include "DataFormats/PatCandidates/interface/PackedCandidate.h"
 
 typedef PFCandidateHistogrammer<reco::PFCandidate> RecoPFCandidateHistogrammer;
-typedef PFCandidateHistogrammer<pat::PackedCandidate> PATPackedCandidateHistogrammer;
-
 DEFINE_FWK_MODULE(RecoPFCandidateHistogrammer);
+
+typedef PFCandidateHistogrammer<pat::PackedCandidate> PATPackedCandidateHistogrammer;
 DEFINE_FWK_MODULE(PATPackedCandidateHistogrammer);
