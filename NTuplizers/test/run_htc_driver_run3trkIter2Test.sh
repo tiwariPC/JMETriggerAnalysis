639dd089f8cd725fbc5b4f753cb096a27119074e
Marino Missiroli <marino.missiroli@desy.de>
2020-03-26 21:22:42 +0100

    [NTuplizers] updated test/run_htc_driver_run3trkIter2Test.sh

--- NTuplizers/test/run_htc_driver_run3trkIter2Test.sh
+++ NTuplizers/test/run_htc_driver_run3trkIter2Test.sh
@@ -9,17 +9,24 @@ fi
 
 NEVT=50000
 
-ODIR=$1
+OUTDIR_ROOT=${1}
+OUTDIR_JSON=${1}_json
 
-JDIR=${1}_json
+if [ ! -d ${OUTDIR_JSON} ]; then
 
-if [ ! -d ${JDIR} ]; then
+  mkdir -p ${OUTDIR_JSON}
 
-  mkdir -p ${JDIR}
+  das_jsondump -v -m ${NEVT} \
+   -d /QCD_Pt-15to3000_TuneCP5_Flat_14TeV_pythia8/Run3Winter20DRMiniAOD-DRFlatPU30to80_110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW \
+   -o ${OUTDIR_JSON}/Run3Winter20_QCD_Pt_15to3000_Flat_14TeV.json
+
+  das_jsondump -v \
+   -d /QCD_Pt_50to80_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW \
+   -o ${OUTDIR_JSON}/Run3Winter20_QCD_Pt_50to80_14TeV.json
 
   das_jsondump -v \
    -d /QCD_Pt_170to300_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW \
-   -o ${JDIR}/Run3Winter20_QCD_Pt_170to300_14TeV.json
+   -o ${OUTDIR_JSON}/Run3Winter20_QCD_Pt_170to300_14TeV.json
 fi
 
 recos=(
@@ -33,21 +40,31 @@ recos=(
 
 for reco in "${recos[@]}"; do
 
-  ODIR=$1/${reco}
+  OUTDIR_ROOT=$1/${reco}
 
-  if [ -d ${ODIR} ]; then
-    printf "%s\n" ">>> output directory already exists: ${ODIR}"
+  if [ -d ${OUTDIR_ROOT} ]; then
+    printf "%s\n" ">>> output directory already exists: ${OUTDIR_ROOT}"
     continue
   fi
 
-  htc_driver -c jmeTriggerNTuple_cfg.py -n 200 numThreads=1 --cpus 1 --memory 3000 --runtime 10800 \
-   -d ${JDIR}/Run3Winter20_QCD_Pt_170to300_14TeV.json -p 0 \
-   -o ${ODIR}/Run3Winter20_QCD_Pt_170to300_14TeV \
+  htc_driver -c jmeTriggerNTuple_cfg.py -n 2000 numThreads=1 --cpus 1 --memory 3000 --runtime 10800 \
+   -d ${OUTDIR_JSON}/Run3Winter20_QCD_Pt_15to3000_Flat_14TeV.json -p 0 \
+   -o ${OUTDIR_ROOT}/Run3Winter20_QCD_Pt_15to3000_Flat_14TeV \
    -m ${NEVT} reco=${reco}
 
-  unset -v ODIR
+  htc_driver -c jmeTriggerNTuple_cfg.py -n 2000 numThreads=1 --cpus 1 --memory 3000 --runtime 10800 \
+   -d ${OUTDIR_JSON}/Run3Winter20_QCD_Pt_50to80_14TeV.json -p 0 \
+   -o ${OUTDIR_ROOT}/Run3Winter20_QCD_Pt_50to80_14TeV \
+   -m ${NEVT} reco=${reco}
+
+  htc_driver -c jmeTriggerNTuple_cfg.py -n 2000 numThreads=1 --cpus 1 --memory 3000 --runtime 10800 \
+   -d ${OUTDIR_JSON}/Run3Winter20_QCD_Pt_170to300_14TeV.json -p 0 \
+   -o ${OUTDIR_ROOT}/Run3Winter20_QCD_Pt_170to300_14TeV \
+   -m ${NEVT} reco=${reco}
+
+  unset -v OUTDIR_ROOT
 
 done
 unset -v reco
 
-unset -v NEVT JDIR
+unset -v NEVT OUTDIR_JSON
