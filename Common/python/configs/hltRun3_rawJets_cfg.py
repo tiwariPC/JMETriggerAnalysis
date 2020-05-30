3814e0ee58a74870ac9167ca126b469e2ea0f9d1
Marino Missiroli <marino.missiroli@desy.de>
2020-05-30 20:09:13 +0200

    [Common] added python/configs/hltRun3_rawJets_cfg.py

---
+++ Common/python/configs/hltRun3_rawJets_cfg.py
@@ -0,0 +1,132 @@
+###
+### command-line arguments
+###
+import FWCore.ParameterSet.VarParsing as vpo
+opts = vpo.VarParsing('analysis')
+
+opts.register('skipEvents', 0,
+              vpo.VarParsing.multiplicity.singleton,
+              vpo.VarParsing.varType.int,
+              'number of events to be skipped')
+
+opts.register('dumpPython', None,
+              vpo.VarParsing.multiplicity.singleton,
+              vpo.VarParsing.varType.string,
+              'path to python file with content of cms.Process')
+
+opts.register('numThreads', 1,
+              vpo.VarParsing.multiplicity.singleton,
+              vpo.VarParsing.varType.int,
+              'number of threads')
+
+opts.register('numStreams', 1,
+              vpo.VarParsing.multiplicity.singleton,
+              vpo.VarParsing.varType.int,
+              'number of streams')
+
+opts.register('wantSummary', False,
+              vpo.VarParsing.multiplicity.singleton,
+              vpo.VarParsing.varType.bool,
+              'show cmsRun summary at job completion')
+
+opts.register('globalTag', '110X_mcRun3_2021_realistic_Candidate_2020_05_26_16_08_15',
+              vpo.VarParsing.multiplicity.singleton,
+              vpo.VarParsing.varType.string,
+              'argument of process.GlobalTag.globaltag')
+
+opts.register('reco', 'HLT_singleTrkIterWithPatatrack_v01_pixVtxFrac0p30',
+              vpo.VarParsing.multiplicity.singleton,
+              vpo.VarParsing.varType.string,
+              'keyword defining reconstruction methods for JME inputs')
+
+opts.register('output', 'out.root',
+              vpo.VarParsing.multiplicity.singleton,
+              vpo.VarParsing.varType.string,
+              'path to output ROOT file')
+
+opts.parseArguments()
+
+###
+### HLT configuration
+###
+from JMETriggerAnalysis.Common.customisedHLTProcess import *
+cms, process = customisedHLTProcess(opts.reco)
+
+# remove cms.OutputModule objects from HLT config-dump
+for _modname in process.outputModules_():
+    _mod = getattr(process, _modname)
+    if type(_mod) == cms.OutputModule:
+       process.__delattr__(_modname)
+       print '> removed cms.OutputModule:', _modname
+
+# remove cms.EndPath objects from HLT config-dump
+for _modname in process.endpaths_():
+    _mod = getattr(process, _modname)
+    if type(_mod) == cms.EndPath:
+       process.__delattr__(_modname)
+       print '> removed cms.EndPath:', _modname
+
+# remove selected cms.Path objects from HLT config-dump
+for _modname in process.paths_():
+    if _modname.startswith('MC_') and ('Jets' in _modname):
+       continue
+    _mod = getattr(process, _modname)
+    if type(_mod) == cms.Path:
+       process.__delattr__(_modname)
+       print '> removed cms.Path:', _modname
+
+# delete process.MessaggeLogger from HLT config
+if hasattr(process, 'MessageLogger'):
+   del process.MessageLogger
+
+# update process.GlobalTag.globaltag
+if opts.globalTag is not None:
+   process.GlobalTag.globaltag = opts.globalTag
+
+# max number of events to be processed
+process.maxEvents.input = opts.maxEvents
+
+# number of events to be skipped
+process.source.skipEvents = cms.untracked.uint32(opts.skipEvents)
+
+# multi-threading settings
+process.options.numberOfThreads = cms.untracked.uint32(opts.numThreads if (opts.numThreads > 1) else 1)
+process.options.numberOfStreams = cms.untracked.uint32(opts.numStreams if (opts.numStreams > 1) else 1)
+if hasattr(process, 'DQMStore'):
+   process.DQMStore.enableMultiThread = (process.options.numberOfThreads > 1)
+
+# show cmsRun summary at job completion
+process.options.wantSummary = cms.untracked.bool(opts.wantSummary)
+
+# EDM input
+if opts.inputFiles:
+   process.source.fileNames = opts.inputFiles
+else:
+   process.source.fileNames = [
+     '/store/mc/Run3Winter20DRMiniAOD/QCD_Pt-15to3000_TuneCP5_Flat_14TeV_pythia8/GEN-SIM-RAW/DRFlatPU30to80_110X_mcRun3_2021_realistic_v6-v2/50000/1E007C6B-0236-774C-AE76-16FF40129ED8.root',
+   ]
+
+# EDM output
+process.HLToutput = cms.OutputModule('PoolOutputModule',
+  dataset = cms.untracked.PSet(
+    dataTier = cms.untracked.string('HLT'),
+    filterName = cms.untracked.string('')
+  ),
+  fileName = cms.untracked.string(opts.output),
+  outputCommands = cms.untracked.vstring((
+    'drop *',
+    'keep GenEventInfoProduct_generator_*_*',
+    'keep *_addPileupInfo_*_*',
+    'keep *_fixedGridRhoFastjetAll*_*_*',
+    'keep *_hltFixedGridRhoFastjetAll*_*_*',
+    'keep *_hlt*Jets__*',
+    'keep *_ak*GenJets__*',
+    'keep *_ak*GenJetsNoNu__*',
+  )),
+  splitLevel = cms.untracked.int32(0)
+)
+process.HLToutput_step = cms.EndPath(process.HLToutput)
+
+# dump content of cms.Process to python file
+if opts.dumpPython is not None:
+   open(opts.dumpPython, 'w').write(process.dumpPython())
