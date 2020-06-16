e94bbf198d9c46567c429b18c8805ebb7cfd9240
Marino Missiroli <marino.missiroli@desy.de>
2020-06-16 17:04:40 +0200

    [NTuplizers] updated test/scripts/makeNTuples_run2_102X_checkHLTJECs.sh

--- NTuplizers/test/scripts/check_hltJECs/plotsFromTree_AK4PF.sh
+++ NTuplizers/test/scripts/check_hltJECs/plotsFromTree_AK4PF.sh
@@ -2,15 +2,21 @@
 
 set -e
 
-python -B histosFromTree_AK4PF.py output_run3_checkJECs_v0 10
-python -B histosFromTree_AK4PF.py output_run3_checkJECs_v1 10
+IDIR1=tmp/output_oldGT/QCD_Pt-15to7000_Flat2018
+IDIR2=tmp/output_newGT/QCD_Pt-15to7000_Flat2018
+ODIR=tmpout
 
-rm -rf tmpout
+python -B ${CMSSW_BASE}/src/JMETriggerAnalysis/NTuplizers/test/scripts/check_hltJECs/histosFromTree_AK4PF.py ${IDIR1} 1
+python -B ${CMSSW_BASE}/src/JMETriggerAnalysis/NTuplizers/test/scripts/check_hltJECs/histosFromTree_AK4PF.py ${IDIR2} 1
 
-../../../NTupleAnalysis/JMETrigger/test/test/compareHistos.py \
-  -i output_run3_checkJECs_v0.root:'Before Fix (2016 HLT-JECs)':1 \
-     output_run3_checkJECs_v1.root:'After Fix (2017 HLT-JECs)':2 \
-  -o tmpout \
-  -l '[Run3Winter20] QCD_Pt15-3000_Flat' \
+rm -rf ${ODIR}
+
+${CMSSW_BASE}/src/NTupleAnalysis/JMETrigger/test/compareHistos.py \
+  -i ${IDIR1}.root:'Before Fix (2016 HLT-JECs)':1 \
+     ${IDIR2}.root:'After Fix (2017 HLT-JECs)':2 \
+  -l '[102X, 2018, Pre-Legacy] QCD_Pt15-7000_Flat' \
+  -o ${ODIR} \
   -e pdf root \
   -k None
+
+unset IDIR1 IDIR2 ODIR
