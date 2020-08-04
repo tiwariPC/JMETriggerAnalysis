4f35c45f29cbfddd4e2da196bd22d990bf155968
Marino Missiroli <m.missiroli@cern.ch>
2020-08-04 23:21:53 +0200

    [NTuplizers] updated test/test_wrapper.sh

--- NTuplizers/test/test_wrapper.sh
+++ NTuplizers/test/test_wrapper.sh
@@ -11,11 +11,15 @@ for nnn in {0..3}; do
   printf "%s\n" "old_${nnn}"
   ./test.sh 0 old_${nnn}.root &> old_${nnn}.txt
   grep -inr hltPFPuppi old_${nnn}.txt | grep FastReport
+  rm old_${nnn}.root.txt
   printTTree -i old_${nnn}.root -o old_${nnn}.root.txt
 done; unset nnn;
 
 cd ${CMSSW_BASE}/src
-git checkout devel_puppi01_112X
+#git checkout devel_puppi01_112X
+#git checkout devel_puppi01_112X_float
+git checkout devel_puppi01_112X_float_test2
+#git checkout devel_puppi01_112X_idxs
 scram b &> /dev/null
 cd ${CMSSW_BASE}/src/JMETriggerAnalysis/NTuplizers/test
 printf "%s\n" "--- TARG ---"
@@ -23,5 +27,6 @@ for nnn in {0..3}; do
   printf "%s\n" "new_${nnn}"
   ./test.sh 0 new_${nnn}.root &> new_${nnn}.txt
   grep -inr hltPFPuppi new_${nnn}.txt | grep FastReport
+  rm new_${nnn}.root.txt
   printTTree -i new_${nnn}.root -o new_${nnn}.root.txt
 done; unset nnn;
