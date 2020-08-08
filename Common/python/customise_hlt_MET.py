1566db2901826a5a809f1975e93868c1d7b1661f
Marino Missiroli <m.missiroli@cern.ch>
2020-08-08 17:05:46 +0200

    [Common] added python/customise_hlt_MET.py

---
+++ Common/python/customise_hlt_MET.py
@@ -0,0 +1,49 @@
+import FWCore.ParameterSet.Config as cms
+
+from CommonTools.PileupAlgos.Puppi_cff import puppiNoLep as _puppiNoLep
+from FWCore.ParameterSet.MassReplace import massReplaceInputTag
+from HLTrigger.Configuration.common import producers_by_type
+
+def customise_replacePFMETWithPuppiMETBasedOnPatatrackPixelVertices(process):
+    process.hltPuppiNoLep = _puppiNoLep.clone(
+      candName = 'hltParticleFlow',
+      vertexName = 'hltPixelVertices',
+      UseFromPVLooseTight = True,
+      vtxNdofCut = 0,
+    )
+
+    process.hltPuppiMET = cms.EDProducer('PFMETProducer',
+      alias = cms.string(''),
+      applyWeight = cms.bool(True),
+      calculateSignificance = cms.bool(False),
+      globalThreshold = cms.double(0.0),
+      parameters = cms.PSet(),
+      src = cms.InputTag('hltParticleFlow'),
+      srcWeights = cms.InputTag('hltPuppiNoLep')
+    )
+
+    process.hltPuppiMETSequence = cms.Sequence(
+        process.hltPuppiNoLep
+      + process.hltPuppiMET
+    )
+
+    process = massReplaceInputTag(process, 'hltPFMETProducer', 'hltPuppiMET')
+
+    for path_i in process.paths_():
+        getattr(process, path_i).replace(process.hltPFMETProducer, process.hltPuppiMETSequence)
+
+    # delete PFMET
+    del process.hltPFMETProducer
+
+    # retuning of Puppi parameters for HLT inputs (currently dummy)
+    _nPVfit_p0 = 0.0 # dummy
+    _nPVfit_p1 = 1.0 # dummy
+    for mod_i in producers_by_type(process, 'PuppiProducer'):
+       for algo_idx in range(len(mod_i.algos)):
+          if len(mod_i.algos[algo_idx].MinNeutralPt) != len(mod_i.algos[algo_idx].MinNeutralPtSlope):
+             raise RuntimeError('instance of PuppiProducer is misconfigured:\n\n'+str(mod_i)+' = '+mod_i.dumpPython())
+          for algoReg_idx in range(len(mod_i.algos[algo_idx].MinNeutralPt)):
+             mod_i.algos[algo_idx].MinNeutralPt[algoReg_idx] += _nPVfit_p0 * mod_i.algos[algo_idx].MinNeutralPtSlope[algoReg_idx]
+             mod_i.algos[algo_idx].MinNeutralPtSlope[algoReg_idx] *= _nPVfit_p1
+
+    return process
