30d85781ed1e7e7572a552108bfdfcf8ee5f5519
Marino Missiroli <m.missiroli@cern.ch>
2020-09-11 14:25:09 +0200

    bugfix in setup_cmssw.sh

--- setup_cmssw.sh
+++ setup_cmssw.sh
@@ -13,11 +13,11 @@ cd CMSSW_11_1_2_patch3/src
 eval `scramv1 runtime -sh`
 
 # updates to use l1t::PFJet with HLT plugins
-git cms-merge-topic missirol:devel_hltPhase2_l1tPFJet_1112pa3
+git cms-merge-topic missirol:devel_hltPhase2_l1tPFJet_1112pa3 -u
 
 # # workaround for PFSimParticle::trackerSurfaceMomentum
 # # ref: hatakeyamak:FBaseSimEvent_ProtectAgainstMissingTrackerSurfaceMomentum
 git cms-addpkg FastSimulation/Event
 git remote add hatakeyamak https://github.com/hatakeyamak/cmssw.git
 git fetch hatakeyamak
-git cherry-pick 0cf67551731c80dc85130e4b8ec73c8f44d53cb0^ 0cf67551731c80dc85130e4b8ec73c8f44d53cb0 | git apply
+git diff 0cf67551731c80dc85130e4b8ec73c8f44d53cb0^ 0cf67551731c80dc85130e4b8ec73c8f44d53cb0 | git apply
