92df04fa24244381089f0264a772a9d7205fd83d
Marino Missiroli <marino.missiroli@cern.ch>
2020-09-13 08:46:35 +0200

    [NTuplizers] updated test/scripts/makeNTuples_hltPhase2_200909.sh

--- NTuplizers/test/scripts/makeNTuples_hltPhase2_200909.sh
+++ NTuplizers/test/scripts/makeNTuples_hltPhase2_200909.sh
@@ -7,7 +7,7 @@ if [ $# -lt 1 ]; then
   exit 1
 fi
 
-NEVT=50000
+NEVT=-1 #50000
 
 ODIR=$1
 
@@ -17,10 +17,11 @@ if [ -d ${ODIR} ]; then
 fi
 
 declare -A samplesMap
-#samplesMap["Phase2HLTTDR_QCD_Pt_15to3000_Flat_14TeV_NoPU"]="/QCD_Pt-15to3000_TuneCP5_Flat_14TeV-pythia8/Phase2HLTTDRSummer20ReRECOMiniAOD-NoPU_castor_all_pt_tracks_111X_mcRun4_realistic_T15_v1-v1/FEVT"
-samplesMap["Phase2HLTTDR_QCD_Pt_15to3000_Flat_14TeV_PU200"]="/QCD_Pt-15to3000_TuneCP5_Flat_14TeV-pythia8/Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_castor_111X_mcRun4_realistic_T15_v1-v1/FEVT"
-#samplesMap["Phase2HLTTDR_VBF_HToInvisible_14TeV_NoPU"]="/VBF_HToInvisible_M125_TuneCUETP8M1_14TeV_powheg_pythia8/Phase2HLTTDRSummer20ReRECOMiniAOD-NoPU_111X_mcRun4_realistic_T15_v1-v1/FEVT"
-samplesMap["Phase2HLTTDR_VBF_HToInvisible_14TeV_PU200"]="/VBF_HToInvisible_M125_14TeV_powheg_pythia8_TuneCP5/Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_111X_mcRun4_realistic_T15_v1-v1/FEVT"
+samplesMap["Phase2HLTTDR_MinBiasPt_14TeV_PU200"]="/MinBias_TuneCP5_14TeV-pythia8/Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_111X_mcRun4_realistic_T15_v1-v1/FEVT"
+##samplesMap["Phase2HLTTDR_QCD_Pt_15to3000_Flat_14TeV_NoPU"]="/QCD_Pt-15to3000_TuneCP5_Flat_14TeV-pythia8/Phase2HLTTDRSummer20ReRECOMiniAOD-NoPU_castor_all_pt_tracks_111X_mcRun4_realistic_T15_v1-v1/FEVT"
+#samplesMap["Phase2HLTTDR_QCD_Pt_15to3000_Flat_14TeV_PU200"]="/QCD_Pt-15to3000_TuneCP5_Flat_14TeV-pythia8/Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_castor_111X_mcRun4_realistic_T15_v1-v1/FEVT"
+##samplesMap["Phase2HLTTDR_VBF_HToInvisible_14TeV_NoPU"]="/VBF_HToInvisible_M125_TuneCUETP8M1_14TeV_powheg_pythia8/Phase2HLTTDRSummer20ReRECOMiniAOD-NoPU_111X_mcRun4_realistic_T15_v1-v1/FEVT"
+#samplesMap["Phase2HLTTDR_VBF_HToInvisible_14TeV_PU200"]="/VBF_HToInvisible_M125_14TeV_powheg_pythia8_TuneCP5/Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_111X_mcRun4_realistic_T15_v1-v1/FEVT"
 
 recoKeys=(
 #  HLT_TRKv00
@@ -35,9 +36,7 @@ recoKeys=(
 
 JDIR=${1}_json
 
-if [ ! -d ${JDIR} ]; then
-  mkdir -p ${JDIR}
-fi
+mkdir -p ${JDIR}
 
 for sample_key in ${!samplesMap[@]}; do
   sample_name=${samplesMap[${sample_key}]}
@@ -52,10 +51,10 @@ for sample_key in ${!samplesMap[@]}; do
       -o ${ODIR}/${reco_key}/${sample_key} \
       -m ${NEVT} reco=${reco_key} globalTag=111X_mcRun4_realistic_T15_v2 trkdqm=1 pfdqm=2
   done
-  unset -v reco_key
-  unset -v sample_name
+  unset reco_key
+  unset sample_name
 done
-unset -v sample_key
+unset sample_key
 
 unset NEVT ODIR JDIR
 unset recoKeys samplesMap
