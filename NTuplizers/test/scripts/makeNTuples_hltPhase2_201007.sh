0dba54a44c35001572f4731d2bf807d344cac10f
Marino Missiroli <m.missiroli@cern.ch>
2020-10-13 16:19:26 +0200

    [NTuplizers] updated test/scripts/makeNTuples_hltPhase2_201007.sh

--- NTuplizers/test/scripts/makeNTuples_hltPhase2_201007.sh
+++ NTuplizers/test/scripts/makeNTuples_hltPhase2_201007.sh
@@ -7,7 +7,7 @@ if [ $# -lt 1 ]; then
   exit 1
 fi
 
-NEVT=50000
+NEVT=500000
 
 ODIR=$1
 
@@ -17,11 +17,11 @@ if [ -d ${ODIR} ]; then
 fi
 
 declare -A samplesMap
-#samplesMap["Phase2HLTTDR_MinBias_14TeV_PU200"]="/MinBias_TuneCP5_14TeV-pythia8/Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_111X_mcRun4_realistic_T15_v1-v1/FEVT"
+samplesMap["Phase2HLTTDR_MinBias_14TeV_PU200"]="/MinBias_TuneCP5_14TeV-pythia8/Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_111X_mcRun4_realistic_T15_v1-v1/FEVT"
 #samplesMap["Phase2HLTTDR_QCD_Pt_15to3000_Flat_14TeV_NoPU"]="/QCD_Pt-15to3000_TuneCP5_Flat_14TeV-pythia8/Phase2HLTTDRSummer20ReRECOMiniAOD-NoPU_castor_all_pt_tracks_111X_mcRun4_realistic_T15_v1-v1/FEVT"
-samplesMap["Phase2HLTTDR_QCD_Pt_15to3000_Flat_14TeV_PU200"]="/QCD_Pt-15to3000_TuneCP5_Flat_14TeV-pythia8/Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_castor_111X_mcRun4_realistic_T15_v1-v1/FEVT"
+#samplesMap["Phase2HLTTDR_QCD_Pt_15to3000_Flat_14TeV_PU200"]="/QCD_Pt-15to3000_TuneCP5_Flat_14TeV-pythia8/Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_castor_111X_mcRun4_realistic_T15_v1-v1/FEVT"
 #samplesMap["Phase2HLTTDR_VBF_HToInvisible_14TeV_NoPU"]="/VBF_HToInvisible_M125_TuneCUETP8M1_14TeV_powheg_pythia8/Phase2HLTTDRSummer20ReRECOMiniAOD-NoPU_111X_mcRun4_realistic_T15_v1-v1/FEVT"
-samplesMap["Phase2HLTTDR_VBF_HToInvisible_14TeV_PU200"]="/VBF_HToInvisible_M125_14TeV_powheg_pythia8_TuneCP5/Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_111X_mcRun4_realistic_T15_v1-v1/FEVT"
+#samplesMap["Phase2HLTTDR_VBF_HToInvisible_14TeV_PU200"]="/VBF_HToInvisible_M125_14TeV_powheg_pythia8_TuneCP5/Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_111X_mcRun4_realistic_T15_v1-v1/FEVT"
 
 recoKeys=(
 #  HLT_TRKv00
@@ -30,13 +30,13 @@ recoKeys=(
 #  HLT_TRKv02_TICL
   HLT_TRKv06
   HLT_TRKv06_TICL
-  HLT_TRKv06_TICL2
-  HLT_TRKv06p1
-  HLT_TRKv06p1_TICL
-  HLT_TRKv06p1_TICL2
+#  HLT_TRKv06_TICL2
+#  HLT_TRKv06p1
+#  HLT_TRKv06p1_TICL
+#  HLT_TRKv06p1_TICL2
   HLT_TRKv07p2
   HLT_TRKv07p2_TICL
-  HLT_TRKv07p2_TICL2
+#  HLT_TRKv07p2_TICL2
 )
 
 for sampleKey in ${!samplesMap[@]}; do
