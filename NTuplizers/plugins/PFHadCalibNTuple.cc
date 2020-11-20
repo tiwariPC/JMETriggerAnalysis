0c7b8ea80e68ec9218cce439d647257fe534bf6c
Marino Missiroli <m.missiroli@cern.ch>
2020-11-20 20:34:04 +0100

    [NTuplizers] updated plugins/PFHadCalibNTuple.cc

--- NTuplizers/plugins/PFHadCalibNTuple.cc
+++ NTuplizers/plugins/PFHadCalibNTuple.cc
@@ -1,11 +1,10 @@
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "FWCore/Framework/interface/EventSetup.h"
-
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
 #include "DataFormats/Math/interface/deltaR.h"
@@ -20,27 +19,27 @@
 #include <TFile.h>
 #include <TTree.h>
 
-class PFHadCalibNTuple : public edm::EDAnalyzer {
-  typedef std::vector<reco::GenParticle> GenParticleCollection;
-  typedef std::vector<reco::PFSimParticle> PFSimParticleCollection;
-  typedef std::vector<reco::PFCandidate> HltParticleFlow;
+class PFHadCalibNTuple : public edm::one::EDAnalyzer<edm::one::SharedResources> {
 
 public:
-  explicit PFHadCalibNTuple(const edm::ParameterSet &);
+  explicit PFHadCalibNTuple(edm::ParameterSet const&);
   ~PFHadCalibNTuple() override;
 
   static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
 
 private:
-  void beginJob() override { Book_trees(); }
   void analyze(const edm::Event &, const edm::EventSetup &) override;
 
   edm::EDGetTokenT<reco::PFCandidateCollection> hltpfcandTag;
   edm::EDGetTokenT<reco::PFSimParticleCollection> PFSimParticlesTag;
   edm::EDGetTokenT<reco::GenParticleCollection> genParInfoTag;
 
-  void Book_trees();
-  void Reset_variables();
+  TTree* ttree_ = nullptr;
+
+  std::string const ttreeName_;
+
+  /// use PFBlockElements to find tracks associated to PFChargedHadron
+  bool const usePFBlockElements_;
 
   /// Min pt for charged hadrons
   double ptMin_;
@@ -61,43 +60,34 @@ private:
   std::vector<int> nHitMin_;
   std::vector<double> nEtaMin_;
 
-  // Number of tracks after cuts
-  std::vector<unsigned int> nCh;
-  std::vector<unsigned int> nEv;
+  /// Number of tracks after cuts
+  std::vector<uint> nCh;
+  std::vector<uint> nEv;
 
-  std::string outputfile_;
-  TFile *tf1;
-  TTree *s;
+  std::vector<float> true_energy_;
+  std::vector<float> true_eta_;
+  std::vector<float> true_phi_;
+  std::vector<float> true_dr_;
+  std::vector<bool> true_isCharged_;
+  std::vector<float> pfc_ecal_;
+  std::vector<float> pfc_hcal_;
+  std::vector<float> pfc_eta_;
+  std::vector<float> pfc_phi_;
+  std::vector<float> pfc_charge_;
+  std::vector<float> pfc_id_;
+  std::vector<float> trackRef_p_;
 
-  int charge_;
-  float true_, p_, ecal_, hcal_, eta_, phi_, ho_;
-  float gen_E_, gen_pt_, gen_eta_, gen_phi_;
-  std::vector<float> true_energy;
-  std::vector<float> true_eta;
-  std::vector<float> true_phi;
-  std::vector<float> true_dr;
-  std::vector<bool> true_isCharged;
-  std::vector<float> pfc_ecal;
-  std::vector<float> pfc_hcal;
-  std::vector<float> pfc_eta;
-  std::vector<float> pfc_phi;
-  std::vector<float> pfc_charge;
-  std::vector<float> pfc_id;
-  std::vector<float> trackRef_p;
-
-  bool isCharged;
+  void reset_variables();
 };
 
-PFHadCalibNTuple::PFHadCalibNTuple(const edm::ParameterSet& iConfig) {
-  nCh = std::vector<unsigned int>(10, static_cast<unsigned int>(0));
-  nEv = std::vector<unsigned int>(3, static_cast<unsigned int>(0));
-
-  //now do what ever initialization is needed
-  //hltpfcandTag      = iConfig.getParameter<edm::InputTag>("HLTPFCandidates");
-  //PFSimParticlesTag = iConfig.getParameter<edm::InputTag>("PFSimParticles");
+PFHadCalibNTuple::PFHadCalibNTuple(const edm::ParameterSet& iConfig)
+  : ttreeName_(iConfig.getParameter<std::string>("TTreeName"))
+  , usePFBlockElements_(iConfig.getParameter<bool>("usePFBlockElements"))
+{
+  nCh = std::vector<uint>(10, 0);
+  nEv = std::vector<uint>(3, 0);
 
   hltpfcandTag = consumes<reco::PFCandidateCollection>(iConfig.getParameter<edm::InputTag>("HLTPFCandidates"));
-  //PFSimParticlesTag = consumes<reco::PFCandidateCollection> ( iConfig.getParameter<edm::InputTag>("PFSimParticles") ) ;
   PFSimParticlesTag = consumes<reco::PFSimParticleCollection>(iConfig.getParameter<edm::InputTag>("PFSimParticles"));
   genParInfoTag = consumes<reco::GenParticleCollection>(iConfig.getParameter<edm::InputTag>("genParTag"));
 
@@ -120,130 +110,142 @@ PFHadCalibNTuple::PFHadCalibNTuple(const edm::ParameterSet& iConfig) {
   nHitMin_ = iConfig.getParameter<std::vector<int> >("nHitMin");
   nEtaMin_ = iConfig.getParameter<std::vector<double> >("nEtaMin");
 
-  // The root tuple
-  outputfile_ = iConfig.getParameter<std::string>("rootOutputFile");
-  tf1 = new TFile(outputfile_.c_str(), "RECREATE");
-  s = new TTree("s", " PFCalibration");
+  usesResource(TFileService::kSharedResource);
+
+  edm::Service<TFileService> fs;
+
+  if (not fs) {
+    throw edm::Exception(edm::errors::Configuration, "TFileService is not registered in cfg file");
+  }
+
+  ttree_ = fs->make<TTree>(ttreeName_.c_str(), ttreeName_.c_str());
+
+  if (not ttree_) {
+    throw edm::Exception(edm::errors::Configuration, "failed to create TTree via TFileService::make<TTree>");
+  }
+
+  ttree_->Branch("true_energy", &true_energy_);
+  ttree_->Branch("true_eta", &true_eta_);
+  ttree_->Branch("true_phi", &true_phi_);
+  ttree_->Branch("true_dr", &true_dr_);
+  ttree_->Branch("true_isCharged", &true_isCharged_);
+  ttree_->Branch("pfc_ecal", &pfc_ecal_);
+  ttree_->Branch("pfc_hcal", &pfc_hcal_);
+  ttree_->Branch("pfc_eta", &pfc_eta_);
+  ttree_->Branch("pfc_phi", &pfc_phi_);
+  ttree_->Branch("pfc_charge", &pfc_charge_);
+  ttree_->Branch("pfc_id", &pfc_id_);
+  ttree_->Branch("trackRef_p", &trackRef_p_);
 }
 
 PFHadCalibNTuple::~PFHadCalibNTuple() {
 
-  std::cout << "Total number of events .............. " << nEv[0] << std::endl;
-  std::cout << "Number of isolated pions " << nEv[1] << std::endl;
-  std::cout << "Number of true particles within dR = 0.01 of an isolated pion " << nEv[2] << std::endl;
-  std::cout << "Number of true particles with track matching " << nCh[7] << std::endl;
+  edm::LogVerbatim("") << "Total number of events .............. " << nEv[0] << std::endl;
+  edm::LogVerbatim("") << "Number of isolated pions " << nEv[1] << std::endl;
+  edm::LogVerbatim("") << "Number of true particles within dR = 0.01 of an isolated pion " << nEv[2] << std::endl;
+  edm::LogVerbatim("") << "Number of true particles with track matching " << nCh[7] << std::endl;
 
-  std::cout << "Number of PF candidates ............. " << nCh[0] << std::endl;
-  std::cout << "Number of PF Charged Hadrons......... " << nCh[1] << std::endl;
-  std::cout << " - With pt > " << ptMin_ << " GeV/c ................ " << nCh[2] << std::endl;
-  std::cout << " - With E_HCAL > " << hcalMin_ << " GeV .............. " << nCh[3] << std::endl;
-  std::cout << " - With only 1 track in the block ... " << nCh[4] << std::endl;
-  std::cout << " - With p > " << pMin_ << " GeV/c ................. " << nCh[5] << std::endl;
-  std::cout << " - With at least " << nPixMin_ << " pixel hits ....... " << nCh[6] << std::endl;
-  std::cout << " - With E_ECAL < " << ecalMax_ << " GeV ............ " << nCh[8] << std::endl;
-
-  tf1->cd();
-  s->Write();
-  tf1->Close();
+  edm::LogVerbatim("") << "Number of PF candidates ............. " << nCh[0] << std::endl;
+  edm::LogVerbatim("") << "Number of PF Charged Hadrons......... " << nCh[1] << std::endl;
+  edm::LogVerbatim("") << " - With pt > " << ptMin_ << " GeV/c ................ " << nCh[2] << std::endl;
+  edm::LogVerbatim("") << " - With E_HCAL > " << hcalMin_ << " GeV .............. " << nCh[3] << std::endl;
+  edm::LogVerbatim("") << " - With only 1 track in the block ... " << nCh[4] << std::endl;
+  edm::LogVerbatim("") << " - With p > " << pMin_ << " GeV/c ................. " << nCh[5] << std::endl;
+  edm::LogVerbatim("") << " - With at least " << nPixMin_ << " pixel hits ....... " << nCh[6] << std::endl;
+  edm::LogVerbatim("") << " - With E_ECAL < " << ecalMax_ << " GeV ............ " << nCh[8] << std::endl;
 }
 
 void PFHadCalibNTuple::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
-  Reset_variables();
+  reset_variables();
 
   edm::Handle<reco::GenParticleCollection> genParticles;
   iEvent.getByToken(genParInfoTag, genParticles); /* get genParticle information */
 
-  edm::Handle<HltParticleFlow> hltpf;
+  edm::Handle<reco::PFCandidateCollection> hltpf;
   iEvent.getByToken(hltpfcandTag, hltpf);
 
   edm::Handle<reco::PFSimParticleCollection> trueParticles;
   bool isSimu = iEvent.getByToken(PFSimParticlesTag, trueParticles);
 
   nEv[0]++;
+
   std::vector<int> genPart;
   genPart.clear();
-  if (isSimu) {
-    for (GenParticleCollection::const_iterator itGenPar = genParticles->begin(); itGenPar != genParticles->end();
-         itGenPar++) {
-      float dR_ = -1;
-      float mindR_ = 99;
 
+  if (isSimu) {
+    for (uint genpIdx_i=0; genpIdx_i<genParticles->size(); ++genpIdx_i) {
+      auto const& genp_i = genParticles->at(genpIdx_i);
       // Gen and true particle selection
-      if (itGenPar->status() == 1 && itGenPar->pdgId() == -211) {
-        for (GenParticleCollection::const_iterator jtGenPar = genParticles->begin(); jtGenPar != genParticles->end();
-             jtGenPar++) {
-          if (itGenPar != jtGenPar && itGenPar->status() == 1) {
-            dR_ = reco::deltaR(itGenPar->eta(), itGenPar->phi(), jtGenPar->eta(), jtGenPar->phi());
-            if (dR_ < mindR_)
-              mindR_ = dR_;
+      if (genp_i.status() == 1 and genp_i.pdgId() == -211) {
+        float mindR = 999999.f;
+        for (uint genpIdx_j=0; genpIdx_j != genpIdx_i and genpIdx_j < genParticles->size(); ++genpIdx_j) {
+          auto const& genp_j = genParticles->at(genpIdx_j);
+          if (genp_i.status() == 1) { //!!
+            float const dR = reco::deltaR(genp_i.eta(), genp_i.phi(), genp_j.eta(), genp_j.phi());
+            if (dR < mindR) mindR = dR;
           }
         }
 
-        if (mindR_ > 1.) {  // Pion isolation
+        if (mindR > 1.) {  // Pion isolation
           nEv[1]++;
-          //Etrue_.clear(); pfc_ecal.clear(); pfc_hcal.clear(); pfc_dr.clear();
 
-          for (int itTruePar = 0; itTruePar < (int)(*trueParticles).size(); itTruePar++) {
+          for (auto const& ptc : *trueParticles) {
             // Only consider negatively charged particles
-            const reco::PFSimParticle& ptc = (*trueParticles)[itTruePar];
-            if (ptc.charge() > 0)
+            if (ptc.charge() > 0) //!!
               continue;
 
             // There should be true particle within deltaR = 0.01 of the gen particle
             const reco::PFTrajectoryPoint& gen = ptc.trajectoryPoint(reco::PFTrajectoryPoint::ClosestApproach);
-            dR_ = reco::deltaR(itGenPar->eta(), itGenPar->phi(), gen.momentum().Eta(), gen.momentum().Phi());
-            if (dR_ > 0.01)
+            float const dR = reco::deltaR(genp_i.eta(), genp_i.phi(), gen.momentum().Eta(), gen.momentum().Phi());
+            if (dR > 0.01)
               continue;
             nEv[2]++;
 
             // Check if there is a reconstructed track *in the event*
-            isCharged = false;
-            for (HltParticleFlow::const_iterator ithltPF = hltpf->begin(); ithltPF != hltpf->end(); ithltPF++) {
-              const reco::PFCandidate& pfc = *ithltPF;
-              //std::cout << "Id = " << pfc.particleId() << std::endl;
+            bool isCharged = false;
+            for (auto const& pfc : *hltpf) {
               if (pfc.particleId() < 4) {
                 isCharged = true;
                 break;
               }
             }
+
             reco::PFTrajectoryPoint::LayerType ecalEntrance = reco::PFTrajectoryPoint::ECALEntrance;
             const reco::PFTrajectoryPoint& tpatecal = ptc.extrapolatedPoint(ecalEntrance);
             if (!tpatecal.isValid())
               continue;
-            eta_ = tpatecal.positionREP().Eta();
-            phi_ = tpatecal.positionREP().Phi();
-            true_ = std::sqrt(tpatecal.momentum().Vect().Mag2());
+
+            float const eta = tpatecal.positionREP().Eta();
+            float const phi = tpatecal.positionREP().Phi();
+            float const trueE = std::sqrt(tpatecal.momentum().Vect().Mag2());
 
             // The extrapolated track should be within 0.1 of the true particle
-            if (reco::deltaR(gen.momentum().Eta(), gen.momentum().Phi(), eta_, phi_) > 0.1)
+            if (reco::deltaR(gen.momentum().Eta(), gen.momentum().Phi(), eta, phi) > 0.1)
               continue;
             nCh[7]++;
 
-            true_energy.push_back(true_);
-            true_eta.push_back(eta_);
-            true_phi.push_back(phi_);
-            true_dr.push_back(reco::deltaR(gen.momentum().Eta(), gen.momentum().Phi(), eta_, phi_));
-            true_isCharged.push_back(isCharged);
+            true_energy_.emplace_back(trueE);
+            true_eta_.emplace_back(eta);
+            true_phi_.emplace_back(phi);
+            true_dr_.emplace_back(reco::deltaR(gen.momentum().Eta(), gen.momentum().Phi(), eta, phi));
+            true_isCharged_.emplace_back(isCharged);
           }  // end of true particle loop
-        }    // isolation > 1
-      }      // stable gen particle pdgid = -211
-    }        // end of all gen particles loop
+        } // isolation > 1
+      } // stable gen particle pdgid = -211
+    } // end of all gen particles loop
 
     // Reco pion(pi-) selection
-    for (HltParticleFlow::const_iterator ithltPF = hltpf->begin(); ithltPF != hltpf->end(); ithltPF++) {
-      const reco::PFCandidate& pfc = *ithltPF;
+    for (auto const& pfc : *hltpf) {
       nCh[0]++;
 
-      if (pfc.particleId() != 1)
-        continue;
+      if (pfc.particleId() != 1) continue;
       nCh[1]++;
 
-      if (pfc.pt() < ptMin_)
-        continue;
+      if (pfc.pt() < ptMin_) continue;
       nCh[2]++;
 
-      double ecalRaw = pfc.rawEcalEnergy();
-      double hcalRaw = pfc.rawHcalEnergy();
+      double const ecalRaw = pfc.rawEcalEnergy();
+      double const hcalRaw = pfc.rawHcalEnergy();
       if (ecalRaw + hcalRaw < hcalMin_)
         continue;
       nCh[3]++;
@@ -256,8 +258,8 @@ void PFHadCalibNTuple::analyze(const edm::Event& iEvent, const edm::EventSetup& 
       //PFBlock::LinkData linkData =  blockRef->linkData();
       //const edm::OwnVector<reco::PFBlockElement>& elements = blockRef->elements();
 
-      //unsigned int nTracks = 0;
-      //unsigned iTrack = 999999;
+      //uint nTracks = 0;
+      //uint iTrack = 999999;
 
       //for(unsigned iEle=0; iEle<elements.size(); iEle++) {
       //	PFBlockElement::Type type = elements[iEle].type();
@@ -284,12 +286,12 @@ void PFHadCalibNTuple::analyze(const edm::Event& iEvent, const edm::EventSetup& 
         continue;
       nCh[5]++;
 
-      unsigned int tobN = 0;
-      unsigned int tecN = 0;
-      unsigned int tibN = 0;
-      unsigned int tidN = 0;
-      unsigned int pxbN = 0;
-      unsigned int pxdN = 0;
+      uint tobN = 0;
+      uint tecN = 0;
+      uint tibN = 0;
+      uint tidN = 0;
+      uint pxbN = 0;
+      uint pxdN = 0;
       const reco::HitPattern& hp = pfc.trackRef()->hitPattern();
       switch (pfc.trackRef()->algo()) {
         case reco::TrackBase::hltIter0:
@@ -318,7 +320,7 @@ void PFHadCalibNTuple::analyze(const edm::Event& iEvent, const edm::EventSetup& 
 
       bool trackerHitOK = false;
       double etaMin = 0.;
-      for (unsigned int ieta = 0; ieta < nEtaMin_.size(); ++ieta) {
+      for (uint ieta = 0; ieta < nEtaMin_.size(); ++ieta) {
         if (fabs(eta) < etaMin)
           break;
 
@@ -335,16 +337,16 @@ void PFHadCalibNTuple::analyze(const edm::Event& iEvent, const edm::EventSetup& 
         continue;
       nCh[8]++;
 
-      pfc_ecal.push_back(ecalRaw);
-      pfc_hcal.push_back(hcalRaw);
-      pfc_eta.push_back(pfc.eta());
-      pfc_phi.push_back(pfc.phi());
-      pfc_charge.push_back(pfc.charge());
-      pfc_id.push_back(pfc.particleId());
-      trackRef_p.push_back(p);
+      pfc_ecal_.emplace_back(ecalRaw);
+      pfc_hcal_.emplace_back(hcalRaw);
+      pfc_eta_.emplace_back(pfc.eta());
+      pfc_phi_.emplace_back(pfc.phi());
+      pfc_charge_.emplace_back(pfc.charge());
+      pfc_id_.emplace_back(pfc.particleId());
+      trackRef_p_.emplace_back(p);
     }  // end of pion (pi-) loop
 
-    s->Fill();
+    ttree_->Fill();
   }  // isSimu
 }
 
@@ -354,36 +356,20 @@ void PFHadCalibNTuple::fillDescriptions(edm::ConfigurationDescriptions& descript
   descriptions.addDefault(desc);
 }
 
-/// Booking trees
-void PFHadCalibNTuple::Book_trees() {
-  s->Branch("true_energy", &true_energy);
-  s->Branch("true_eta", &true_eta);
-  s->Branch("true_phi", &true_phi);
-  s->Branch("true_dr", &true_dr);
-  s->Branch("true_isCharged", &true_isCharged);
-  s->Branch("pfc_ecal", &pfc_ecal);
-  s->Branch("pfc_hcal", &pfc_hcal);
-  s->Branch("pfc_eta", &pfc_eta);
-  s->Branch("pfc_phi", &pfc_phi);
-  s->Branch("pfc_charge", &pfc_charge);
-  s->Branch("pfc_id", &pfc_id);
-  s->Branch("trackRef_p", &trackRef_p);
-}
-
 /// Reset variables
-void PFHadCalibNTuple::Reset_variables() {
-  true_energy.clear();
-  true_eta.clear();
-  true_phi.clear();
-  true_dr.clear();
-  true_isCharged.clear();
-  pfc_ecal.clear();
-  pfc_hcal.clear();
-  pfc_eta.clear();
-  pfc_phi.clear();
-  pfc_charge.clear();
-  pfc_id.clear();
-  trackRef_p.clear();
+void PFHadCalibNTuple::reset_variables() {
+  true_energy_.clear();
+  true_eta_.clear();
+  true_phi_.clear();
+  true_dr_.clear();
+  true_isCharged_.clear();
+  pfc_ecal_.clear();
+  pfc_hcal_.clear();
+  pfc_eta_.clear();
+  pfc_phi_.clear();
+  pfc_charge_.clear();
+  pfc_id_.clear();
+  trackRef_p_.clear();
 }
 
 DEFINE_FWK_MODULE(PFHadCalibNTuple);
--- NTuplizers/test/pfHadCalibNTuple_cfg.py
+++ NTuplizers/test/pfHadCalibNTuple_cfg.py
@@ -1,31 +1,32 @@
+###
+### command-line arguments
+###
+import FWCore.ParameterSet.VarParsing as vpo
+opts = vpo.VarParsing('analysis')
+
+opts.register('numThreads', 1,
+              vpo.VarParsing.multiplicity.singleton,
+              vpo.VarParsing.varType.int,
+              'number of threads')
+
+opts.register('numStreams', 0,
+              vpo.VarParsing.multiplicity.singleton,
+              vpo.VarParsing.varType.int,
+              'number of streams')
+
+opts.register('output', 'PFHadronCalibration.root',
+              vpo.VarParsing.multiplicity.singleton,
+              vpo.VarParsing.varType.string,
+              'path to output ROOT file')
+
+opts.parseArguments()
+
+###
+### base configuration file
+###
 from JMETriggerAnalysis.Common.configs.hltPhase2_TRKv06p1_TICL_cfg import cms, process
 
-process.maxEvents.input = 50
-
-process.options.numberOfThreads = 1
-process.options.numberOfStreams = 0
-
-process.source.fileNames = [
-  '/store/mc/Phase2HLTTDRSummer20ReRECOMiniAOD/MultiPion_PT0to200/GEN-SIM-DIGI-RAW-MINIAOD/NoPU_111X_mcRun4_realistic_T15_v1-v1/250000/5749D223-89ED-C448-AB1A-555597E20D98.root',
-]
-
-process.pfHadCalibNTuple = cms.EDAnalyzer('PFHadCalibNTuple',
-  genParTag = cms.InputTag("genParticles"),
-  HLTPFCandidates = cms.InputTag("pfTICL"),
-  PFSimParticles = cms.InputTag("particleFlowSimParticle"),
-
-  ptMin = cms.double(0.01), # Minimum pt
-  pMin = cms.double(0.01),  # Minimum p
-  nPixMin = cms.int32(2), # Nb of pixel hits for pion
-  nHitMin = cms.vint32(8, 8, 8, 8),          # Nb of track hits for pion
-  nEtaMin = cms.vdouble(1.4, 1.6, 2.0, 2.5), #  in these eta ranges
-
-  hcalMin = cms.double(0.0),  # Minimum hcal energy
-  ecalMax = cms.double(1e12), # Maximum ecal energy 
-  rootOutputFile = cms.string('PFHadCalibration.root')
-)
-
-process.particleFlowSimParticle = cms.EDProducer("PFSimParticleProducer",
+process.particleFlowSimParticle = cms.EDProducer('PFSimParticleProducer',
   Fitter = cms.string('KFFittingSmoother'),
   MCTruthMatchingInfo = cms.untracked.bool(False),
   ParticleFilter = cms.PSet(
@@ -38,19 +39,70 @@ process.particleFlowSimParticle = cms.EDProducer("PFSimParticleProducer",
     zMax = cms.double(317.0),
   ),
   Propagator = cms.string('PropagatorWithMaterial'),
-  RecTracks = cms.InputTag("trackerDrivenElectronSeeds"),
+  RecTracks = cms.InputTag('trackerDrivenElectronSeeds'),
   TTRHBuilder = cms.string('WithTrackAngle'),
-  ecalRecHitsEB = cms.InputTag("caloRecHits","EcalRecHitsEB"),
-  ecalRecHitsEE = cms.InputTag("caloRecHits","EcalRecHitsEE"),
-  fastSimProducer = cms.untracked.InputTag("fastSimProducer","EcalHitsEB"),
+  ecalRecHitsEB = cms.InputTag('caloRecHits','EcalRecHitsEB'),
+  ecalRecHitsEE = cms.InputTag('caloRecHits','EcalRecHitsEE'),
+  fastSimProducer = cms.untracked.InputTag('fastSimProducer','EcalHitsEB'),
   process_Particles = cms.untracked.bool(True),
   process_RecTracks = cms.untracked.bool(False),
-  sim = cms.InputTag("g4SimHits"),
+  sim = cms.InputTag('g4SimHits'),
   verbose = cms.untracked.bool(False)
+)
+
+process.pfHadCalibNTuplePFBarrel = cms.EDAnalyzer('PFHadCalibNTuple',
+  genParTag = cms.InputTag('genParticles'),
+  HLTPFCandidates = cms.InputTag('particleFlowTmpBarrel'),
+  PFSimParticles = cms.InputTag('particleFlowSimParticle'),
+
+  usePFBlockElements = cms.bool(True),
+
+  ptMin = cms.double(0.01), # Minimum pt
+  pMin = cms.double(0.01),  # Minimum p
+  nPixMin = cms.int32(2), # Nb of pixel hits for pion
+  nHitMin = cms.vint32(8, 8, 8, 8),          # Nb of track hits for pion
+  nEtaMin = cms.vdouble(1.4, 1.6, 2.0, 2.5), #  in these eta ranges
+
+  hcalMin = cms.double(0.0),  # Minimum hcal energy
+  ecalMax = cms.double(1e12), # Maximum ecal energy
+
+  TTreeName = cms.string('Candidates'),
+)
+
+process.pfHadCalibNTuplePFTICL = process.pfHadCalibNTuplePFBarrel.clone(
+  HLTPFCandidates = 'pfTICL',
+  usePFBlockElements = False,
 )
 
 process.pfSimParticleSeq = cms.Sequence(process.particleFlowSimParticle)
 process.pfSimParticlePath = cms.Path(process.pfSimParticleSeq)
 
-process.pfHadCalibNTupleEndPath = cms.EndPath(process.pfHadCalibNTuple)
-process.setSchedule_(cms.Schedule(process.MC_JME, process.pfSimParticlePath, process.pfHadCalibNTupleEndPath))
+process.pfHadCalibNTupleSeq = cms.Sequence(
+    process.pfHadCalibNTuplePFBarrel
+  + process.pfHadCalibNTuplePFTICL
+)
+process.pfHadCalibNTupleEndPath = cms.EndPath(process.pfHadCalibNTupleSeq)
+
+process.setSchedule_(cms.Schedule(
+  process.MC_JME,
+  process.pfSimParticlePath,
+  process.pfHadCalibNTupleEndPath,
+))
+
+# number of events
+process.maxEvents.input = opts.maxEvents
+
+# number of threads/streams
+process.options.numberOfThreads = opts.numThreads
+process.options.numberOfStreams = opts.numStreams
+
+# input files
+if opts.inputFiles:
+  process.source.fileNames = opts.inputFiles
+else:
+  process.source.fileNames = [
+    '/store/mc/Phase2HLTTDRSummer20ReRECOMiniAOD/MultiPion_PT0to200/GEN-SIM-DIGI-RAW-MINIAOD/NoPU_111X_mcRun4_realistic_T15_v1-v1/250000/5749D223-89ED-C448-AB1A-555597E20D98.root',
+  ]
+
+# output file
+process.TFileService = cms.Service('TFileService', fileName = cms.string(opts.output))
