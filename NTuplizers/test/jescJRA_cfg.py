0b1c6c9e141c9e1403acb99a972bf9d0c8d94d79
Marino Missiroli <m.missiroli@cern.ch>
2021-03-03 21:53:03 +0100

    revert changes to jescJRA_cfg.py

--- NTuplizers/test/jescJRA_cfg.py
+++ NTuplizers/test/jescJRA_cfg.py
@@ -109,6 +109,7 @@ for _modname in sorted(process.paths_()):
       print '{:<99} | {:<4} |'.format(_modname, '')
 print '-'*108
 
+# remove FastTimerService
 if hasattr(process, 'FastTimerService'):
   del process.FastTimerService
 
@@ -147,10 +148,10 @@ process.pfhcESPrefer = cms.ESPrefer('PoolDBESSource', 'pfhcESSource')
 import JetMETAnalysis.JetAnalyzers.DefaultsHLT_cff as Defaults
 from JetMETAnalysis.JetAnalyzers.addAlgorithmHLT import addAlgorithm
 for algorithm in [
-  'ak4puppiHLT',
   'ak4caloHLT',
   'ak4pfclusterHLT',
   'ak4pfHLT',
+  'ak4puppiHLT',
   'ak8caloHLT',
   'ak8pfclusterHLT',
   'ak8pfHLT',
@@ -194,16 +195,10 @@ if opts.inputFiles:
    process.source.fileNames = opts.inputFiles
 else:
    process.source.fileNames = [
-     #'/store/mc/Run3Winter20DRMiniAOD/QCD_Pt-15to7000_TuneCP5_Flat_14TeV_pythia8/GEN-SIM-RAW/NoPU_110X_mcRun3_2021_realistic_v6_ext1-v1/250000/07569899-8384-6444-AD42-613509DEE3E1.root',
-     # SPS  DoublePhoton sample only for testing!
-     '/store/mc/Run3Winter20DRMiniAOD/DoublePhoton_FlatPt-5To300/GEN-SIM-RAW/FlatPU0to80_110X_mcRun3_2021_realistic_v6-v2/40000/7BA83802-8EF6-B246-893F-09A1657B7CC7.root'
+     '/store/mc/Run3Winter20DRMiniAOD/QCD_Pt-15to7000_TuneCP5_Flat_14TeV_pythia8/GEN-SIM-RAW/FlatPU0to80_110X_mcRun3_2021_realistic_v6-v1/100000/07634100-A880-4E4D-BAA3-D9C0B5356C2D.root',
    ]
 
-
-#
-# SPS removing secondaryFileNames - not found when running, not needed?
-#
-#process.source.secondaryFileNames = []
+process.source.secondaryFileNames = []
 
 # create TFileService to be accessed by JRA-NTuple plugin
 process.TFileService = cms.Service('TFileService', fileName = cms.string(opts.output))
@@ -224,70 +219,4 @@ if opts.verbosity > 0:
    print 'process.source =', process.source.dumpPython()
    print 'process.maxEvents =', process.maxEvents.dumpPython()
    print 'process.options =', process.options.dumpPython()
-   print '----------------------------'
-
-###
-### HLT configuration
-###
-if opts.reco == 'HLT_GRun':
-  from JMETriggerAnalysis.Common.configs.HLT_dev_CMSSW_11_2_0_GRun_V19_configDump import cms, process
-
-elif opts.reco == 'HLT_Run3TRK':
-  # (a) Run-3 tracking: standard
-  from JMETriggerAnalysis.Common.configs.HLT_dev_CMSSW_11_2_0_GRun_V19_configDump import cms, process
-  from HLTrigger.Configuration.customizeHLTRun3Tracking import customizeHLTRun3Tracking
-  process = customizeHLTRun3Tracking(process)
-
-elif opts.reco == 'HLT_Run3TRKWithPU':
-  # (b) Run-3 tracking: all pixel vertices
-  from JMETriggerAnalysis.Common.configs.HLT_dev_CMSSW_11_2_0_GRun_V19_configDump import cms, process
-  from HLTrigger.Configuration.customizeHLTRun3Tracking import customizeHLTRun3TrackingAllPixelVertices
-  process = customizeHLTRun3TrackingAllPixelVertices(process)
-
-else:
-  raise RuntimeError('keyword "reco = '+opts.reco+'" not recognised')
-
-# remove cms.OutputModule objects from HLT config-dump
-for _modname in process.outputModules_():
-    _mod = getattr(process, _modname)
-    if type(_mod) == cms.OutputModule:
-       process.__delattr__(_modname)
-       if opts.verbosity > 0:
-          print '> removed cms.OutputModule:', _modname
-
-# remove cms.EndPath objects from HLT config-dump
-for _modname in process.endpaths_():
-    _mod = getattr(process, _modname)
-    if type(_mod) == cms.EndPath:
-       process.__delattr__(_modname)
-       if opts.verbosity > 0:
-          print '> removed cms.EndPath:', _modname
-
-# remove selected cms.Path objects from HLT config-dump
-print '-'*108
-print '{:<99} | {:<4} |'.format('cms.Path', 'keep')
-print '-'*108
-for _modname in sorted(process.paths_()):
-    _keepPath = _modname.startswith('MC_') and ('Jets' in _modname or 'MET' in _modname)
-    _keepPath |= _modname.startswith('MC_ReducedIterativeTracking')
-    if _keepPath:
-      print '{:<99} | {:<4} |'.format(_modname, '+')
-      continue
-    _mod = getattr(process, _modname)
-    if type(_mod) == cms.Path:
-      process.__delattr__(_modname)
-      print '{:<99} | {:<4} |'.format(_modname, '')
-print '-'*108
-
-# remove FastTimerService
-#
-# SPS removing the removal of FastTimeService - not needed? caused crash...
-#
-#del process.FastTimerService
-
-# ###
-# ### customizations
-# ###
-# from JMETriggerAnalysis.Common.customise_hlt import *
-# process = addPath_MC_AK4PFClusterJets(process)
-# process = addPath_MC_AK4PFPuppiJets(process)
+   print '----------------------'
