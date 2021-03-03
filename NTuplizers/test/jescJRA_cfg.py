8f4d4234b2c21fd5f38b7969d6750c4d245d97df
sparedes <santiago.paredes@cern.ch>
2021-03-03 18:30:59 +0100

    testing new collection names

--- NTuplizers/test/jescJRA_cfg.py
+++ NTuplizers/test/jescJRA_cfg.py
@@ -115,7 +115,10 @@ for _modname in sorted(process.paths_()):
 print '-'*108
 
 # remove FastTimerService
-del process.FastTimerService
+#
+# SPS removing the removal of FastTimeService - not needed? caused crash...
+#
+#del process.FastTimerService
 
 # remove MessageLogger
 del process.MessageLogger
@@ -133,13 +136,13 @@ process = addPaths_MC_PFPuppiJME(process)
 import JetMETAnalysis.JetAnalyzers.DefaultsHLT_cff as Defaults
 from JetMETAnalysis.JetAnalyzers.addAlgorithmHLT import addAlgorithm
 for algorithm in [
-  #'ak4caloHLT',
-  #'ak4pfclusterHLT',
-  #'ak4pfHLT',
   'ak4puppiHLT',
-  #'ak8caloHLT',
-  #'ak8pfclusterHLT',
-  #'ak8pfHLT',
+  'ak4caloHLT',
+  'ak4pfclusterHLT',
+  'ak4pfHLT',
+  'ak8caloHLT',
+  'ak8pfclusterHLT',
+  'ak8pfHLT',
   'ak8puppiHLT',
 ]:
   addAlgorithm(process, algorithm, Defaults)
@@ -219,12 +222,18 @@ if opts.logs:
    )
 
 # input EDM files
-process.source.secondaryFileNames = []
+
+#
+# SPS removing secondaryFileNames - not found when running, not needed?
+#
+#process.source.secondaryFileNames = []
 if opts.inputFiles:
    process.source.fileNames = opts.inputFiles
 else:
    process.source.fileNames = [
-     '/store/mc/Run3Winter20DRMiniAOD/QCD_Pt-15to7000_TuneCP5_Flat_14TeV_pythia8/GEN-SIM-RAW/NoPU_110X_mcRun3_2021_realistic_v6_ext1-v1/250000/07569899-8384-6444-AD42-613509DEE3E1.root',
+     #'/store/mc/Run3Winter20DRMiniAOD/QCD_Pt-15to7000_TuneCP5_Flat_14TeV_pythia8/GEN-SIM-RAW/NoPU_110X_mcRun3_2021_realistic_v6_ext1-v1/250000/07569899-8384-6444-AD42-613509DEE3E1.root',
+     # SPS  DoublePhoton sample only for testing!
+     '/store/mc/Run3Winter20DRMiniAOD/DoublePhoton_FlatPt-5To300/GEN-SIM-RAW/FlatPU0to80_110X_mcRun3_2021_realistic_v6-v2/40000/7BA83802-8EF6-B246-893F-09A1657B7CC7.root'
    ]
 
 # dump content of cms.Process to python file
@@ -299,7 +308,10 @@ for _modname in sorted(process.paths_()):
 print '-'*108
 
 # remove FastTimerService
-del process.FastTimerService
+#
+# SPS removing the removal of FastTimeService - not needed? caused crash...
+#
+#del process.FastTimerService
 
 # ###
 # ### customizations
